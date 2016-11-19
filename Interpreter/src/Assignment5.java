a7531c506d4ab329ea6fbd542f2bb2acad3ded99
tianbingleng <tianbingleng@TianbingdeMBP.wv.cc.cmu.edu>
2016-11-18 19:51:49 -0500

    added: 1.execution list, List<ProgramLineObject> executionLineObjects 2.assignment statement checking, scanProgramLineToExecutionLine(programLineObjects); 3. checking case STATEMENT: (done) 	checkStatementValid(lineObject); 4. checking case LOOPSTART: (黄。。帮我。。写不下去了) 	checkLoopStartValid(lineObject); 5. TBD - assignment to value (operation)

--- Interpreter/src/Assignment5.java
+++ Interpreter/src/Assignment5.java
@@ -4,45 +4,178 @@ import java.util.*;
 
 public class Assignment5 {
 	public static void main(String[] args) {
+		String command1 = "a = 3 ;";
+		String command2 = "a=b;";
+		String command3 = "loop x {";
+		String command4 = "}";
+
+		// String lineNum Type
+		// Initialize, waiting Huang to passing the whole arraylist
+		ArrayList<ProgramLineObject> programLineObjects = new ArrayList<>();
+		programLineObjects.add(new ProgramLineObject(ProgramLineType.STATEMENT, 1, command1));
+		programLineObjects.add(new ProgramLineObject(ProgramLineType.STATEMENT, 2, command2));
+		programLineObjects.add(new ProgramLineObject(ProgramLineType.LOOPSTART, 3, command3));
+		programLineObjects.add(new ProgramLineObject(ProgramLineType.LOOPEND, 4, command4));
+
+		// scanProgramLine
+		List<ProgramLineObject> executionLineObjects = scanProgramLineToExecutionLine(programLineObjects);
+		
+
+		for (ProgramLineObject plo : executionLineObjects) {
+			System.out.print(plo.contents);
+			System.out.print(plo.lineNumber);
+			System.out.println(plo.type);
+		}
+
 		HashMap<Character, Value> map = new HashMap<>();
-//		//
-//		Value a = new Value("1");
-//		Value b = new Value("2.0");
-//		Value c = new Value("-03.4");
-//		Value d = new Value("499");
-//		Value e = new Value("0");
-//
-//		Value f = c;
-//		Value g = new Value("6");
-//		g.setValue("0.3");
 
-		// a = "5"
-		// key small case Charcter
+		map.put('a', new Value(".1"));
+		// map.put('b', new Value("2.0"));
+		// map.put('c', new Value("-03.4"));
+		// map.put('d', new Value("499"));
+		// map.put('e', new Value("0"));
+		// map.put('f', map.get('c'));
+		// map.put('g', new Value("6"));
+		// map.get('g').setValue("0.3");
 
-		// a = 3
-		// b = a
-
-		map.put('a', new Value("1"));
-		map.put('b', new Value("2.0"));
-		map.put('c', new Value("-03.4"));
-		map.put('d', new Value("499"));
-		map.put('e', new Value("0"));
-		map.put('f', map.get('c'));
-		map.put('g', new Value("6"));
-		map.get('g').setValue("0.3");
-
-//		List<Value> list = new ArrayList<Value>();
-//		list.add(a);
-//		list.add(b);
-//		list.add(c);
-//		list.add(d);
-//		list.add(e);
-//		list.add(f);
-//		list.add(g);
+		// List<Value> list = new ArrayList<Value>();
+		// list.add(a);
+		// list.add(b);
+		// list.add(c);
+		// list.add(d);
+		// list.add(e);
+		// list.add(f);
+		// list.add(g);
 
 		for (char ch : map.keySet()) {
 			map.get(ch).getValue();
 		}
+	}
+
+	private static List<ProgramLineObject> scanProgramLineToExecutionLine(ArrayList<ProgramLineObject> programLineObjects) {
+
+		List<ProgramLineObject> executionLineObjectsList = new ArrayList<>();
+		for (ProgramLineObject lineObject : programLineObjects) {
+			checkLineTokens(lineObject);
+			executionLineObjectsList.add(lineObject);
+		}
+		return executionLineObjectsList;
+	}
+
+	// return VOID if correct
+	// only stop and exit when error
+	private static void checkLineTokens(ProgramLineObject lineObject) {
+		switch (lineObject.type) {
+		case STATEMENT:
+			checkStatementValid(lineObject);
+		case LOOPSTART:
+			checkLoopStartValid(lineObject);
+		case LOOPEND:
+		default:
+		}
+	}
+
+	// return VOID if correct
+	// only stop and exit when error
+	private static void checkStatementValid(ProgramLineObject lineObject) {
+		boolean validExecutionLine = true;
+		ErrorType errorType = null;
+		int errorLine = 0;
+
+//		System.out.print(lineObject.contents + " ");
+//		System.out.print(lineObject.lineNumber + " ");
+//		System.out.println(lineObject.type);
+//
+		String trimString = lineObject.contents.replace(" ", "");
+//		System.out.println("After token:  " + trimString);
+
+		// count number of "="
+		int numberOfEqual = 0;
+		for (int i = 0; i < trimString.length(); i++) {
+			if (trimString.charAt(i) == '=')
+				numberOfEqual++;
+		}
+
+		if (numberOfEqual != 1) {// a==3
+			errorType = ErrorType.STATEMENT_EQUALSIGN_ERROR;
+			errorLine = lineObject.lineNumber;
+			validExecutionLine = false;
+		} else {
+			String[] tokens = trimString.split("[=;]");
+			if (tokens.length != 2) {// =3 or a= or =
+				errorType = ErrorType.STATEMENT_INVALID_ASSIGNMENT_ERROR;
+				errorLine = lineObject.lineNumber;
+				validExecutionLine = false;
+			} else {// check both tokens
+				// check first token
+				if (tokens[0].length() != 1) {// aa=1
+					errorType = ErrorType.STATEMENT_INVALID_ASSIGNMENT_ERROR;
+					errorLine = lineObject.lineNumber;
+					validExecutionLine = false;
+				} else {// 1=1
+					char c = tokens[0].charAt(0);
+					if (!Character.isLetter(c)) {
+						errorType = ErrorType.STATEMENT_INVALID_ASSIGNMENT_ERROR;
+						errorLine = lineObject.lineNumber;
+						validExecutionLine = false;
+					}
+				}
+				// check second token
+				// length is 1
+				if (tokens[1].length() == 1) {// a=?
+					char c = tokens[1].charAt(0);
+					if ((!Character.isDigit(c)) && (!Character.isLetter(c))) {
+						errorType = ErrorType.STATEMENT_INVALID_ASSIGNMENT_ERROR;
+						errorLine = lineObject.lineNumber;
+						validExecutionLine = false;
+					}
+				}
+				// length is >1
+				else {//a=ab
+					if (!isIntFloat(tokens[1])) {
+						errorType = ErrorType.STATEMENT_INVALID_ASSIGNMENT_ERROR;
+						errorLine = lineObject.lineNumber;
+						validExecutionLine = false;
+					}
+				}
+			}
+		}
+		if (validExecutionLine == false) {
+			System.out.print(errorType);
+			System.out.println(" line:" + errorLine);
+			System.exit(0);
+		}
+	}
+
+	private static void checkLoopStartValid(ProgramLineObject lineObject) {
+
+	}
+
+	private static boolean isIntFloat(String str) {
+		boolean status = true;
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (Character.isDigit(c) || c == '.') {
+				// do nothing
+			} else {
+				status = false;
+				break;
+			}
+		}
+		return status;
+	}
+
+}
+
+class ProgramLineObject {
+	ProgramLineType type;
+	int lineNumber;
+	String contents;
+
+	public ProgramLineObject(ProgramLineType type, int lineNumber, String contents) {
+		this.type = type;
+		this.lineNumber = lineNumber;
+		this.contents = contents;
 	}
 }
 
@@ -82,4 +215,16 @@ class Value {
 
 enum ValueType {
 	IntegerType, FloatType
+}
+
+enum ProgramLineType {
+	LOOPSTART, LOOPEND, STATEMENT
+}
+
+enum Type {
+	PROGRAM, END, LEFTCOMMENT, RIGHTCOMMENT, LEFTBRAC, RIGHTBRAC, LOOP
+}
+
+enum ErrorType {
+	SEMICOLON, PROGRAM, END, LEFTCOMMENT, RIGHTCOMMENT, LEFTBRAC, RIGHTBRAC, LOOP, EXTRASTATEMENT, STATEMENT_EQUALSIGN_ERROR, STATEMENT_INVALID_ASSIGNMENT_ERROR
 }
