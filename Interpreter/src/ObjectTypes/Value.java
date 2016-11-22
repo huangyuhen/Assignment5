576c3d32052e2613551e347b18b25f2de98cbb3a
tianbingleng <tianbingleng@TianbingdeMBP.wv.cc.cmu.edu>
2016-11-22 14:45:48 -0500

    comment

--- Interpreter/src/ObjectTypes/Value.java
+++ Interpreter/src/ObjectTypes/Value.java
@@ -1,28 +1,71 @@
-package ObjectTypes;
+/*****************************************************************************************************
+ * File: Value.java
+ * Course: Data Structures and Algorithms for Engineers
+ * Project: A5
+ * Author: Evil Genius
+ * Versions:
+ *	1.0 November 2016
+ *
+ * Description: This class Value defines the values in supported Integer and Float type and provides
+ * methods for other class to access and modify the object.
+ *
+ * Parameters: int iValue;
+ *             float fValue;
+ *             ValueType type;
+ *
+ * Constructor:
+ *                      Value(String input)
+ *
+ * Internal Functions:
+ *                      void setValue(String input)
+ *                      String getValue()
+ *                      void printValue()
+ *
+ *****************************************************************************************************/
 
+package ObjectTypes;
 import EnumerationTypes.ValueType;
 
 public class Value {
-    public int iValue;
-    public float fValue;
-    public ValueType type;
+    public int iValue;//store as int type if value is in int type
+    public float fValue;//store as float type if value is in float type
+    public ValueType type;//store Value type (IntegerType/FloatType)
 
+    /*********************************************************************
+     * Constructor:  Value(String input)
+     * Description: This constructor will read String as an input to declare.
+     * Parameter List: String input
+     * Return Type: void
+     ***********************************************************************/
     public Value(String input) {
+        //if the input is a float
         if (input.contains(".")) {
+            //convert and define the input String into float type value
             this.fValue = Float.parseFloat(input);
             this.type = ValueType.FloatType;
-        } else {
+        }
+        //if the input is an integer
+        else {
+            //convert and define the input String into int type value
             this.iValue = Integer.parseInt(input);
             this.type = ValueType.IntegerType;
         }
     }
 
-    // override the whole Value
+    /*********************************************************************
+     * Constructor:  setValue(String input)
+     * Description: This method will set/overwrite the Value object.
+     * Parameter List: String input
+     * Return Type: void
+     ***********************************************************************/
     public void setValue(String input) {
+        //if the input is a float
         if (input.contains(".")) {
             this.fValue = Float.parseFloat(input);
             this.type = ValueType.FloatType;
-        } else {
+        }
+        //if the input is an integer
+        else {
             this.iValue = Integer.parseInt(input);
             this.type = ValueType.IntegerType;
         }
