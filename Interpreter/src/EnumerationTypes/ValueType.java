5443a34937221ce1e705b7620a36149af40c9d08
tianbingleng <tianbingleng@TianbingdeMBP.wv.cc.cmu.edu>
2016-11-22 15:20:58 -0500

    add comments for EnumerationTypes (ErrorType, ProgramLineType, Type, ValueType)

--- Interpreter/src/EnumerationTypes/ValueType.java
+++ Interpreter/src/EnumerationTypes/ValueType.java
@@ -1,3 +1,18 @@
+/*****************************************************************************************************
+ * File: ValueType.java
+ * Course: Data Structures and Algorithms for Engineers
+ * Project: A5
+ * Author: Evil Genius
+ * Versions:
+ *	1.0 November 2016
+ *
+ * Description: This EnumerationTypes defines type of Value object.
+ *
+ * ValueType:     IntegerType, FloatType
+ *
+ *****************************************************************************************************/
+
+
 package EnumerationTypes;
 
 public enum ValueType {
