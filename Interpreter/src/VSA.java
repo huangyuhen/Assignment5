f3388d7acdc8adbbfd1279168af94bb5e22e0740
Yuheng Huang <huangyuhen@hotmail.com>
2016-11-22 14:45:23 -0500

    Added command line reading and error statement.

--- Interpreter/src/VSA.java
+++ Interpreter/src/VSA.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class VSA {
-    public static String readFile() {
-        System.out.println("Please enter the full file path with extension name");
-        Scanner reader = new Scanner(System.in);
-        String path = reader.nextLine();
+    public static String readFile(String path) {
         File file = new File(path);
         if (!file.exists()){
             System.out.println("Please enter a existing file.");
@@ -33,13 +30,35 @@ public class VSA {
         return null;
     }
 
+    public static void executionOfProgram(String filePath){
+        SyntaxCheckADT syntaxCheckADT = new SyntaxCheckADT();
+        String fileContents = readFile(filePath);
+        syntaxCheckADT.contentsPreProcessing(fileContents);
+        boolean syntaxCheck = syntaxCheckADT.syntaxCheck();
+        if (syntaxCheck) {
+            Execution a = new Execution(syntaxCheckADT.programLine);
+            a.executeLines();
+        }
+        else
+            System.exit(0);
+    }
+
+    public static String argumentCheck(String[] args){
+        if (args.length > 2) {
+            System.out.println("Too many arguments");
+            System.exit(0);
+        }
+        else if (args.length < 1) {
+            System.out.println("Too few arguments");
+            System.exit(0);
+        }
+        else
+            return args[0];
+        return null;
+    }
     public static void main(String[] args){
-        SyntaxCheckADT i = new SyntaxCheckADT();
-        String filePath = readFile();
-        i.contentsPreProcessing(filePath);
-        i.syntaxCheck();
-        i.programLineProcess();
-        Execution a = new Execution(i.programLine);
-        a.executeLines();
+        String filePath = argumentCheck(args);
+        if (filePath != null)
+            executionOfProgram(filePath);
     }
 }
