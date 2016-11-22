f2cfcae814c70b8522704736ce06e28a732e4457
Yuheng Huang <huangyuhen@hotmail.com>
2016-11-22 17:07:15 -0500

    Added Syntax check ADT

--- Interpreter/src/VSA.java
+++ Interpreter/src/VSA.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+
 
 public class VSA {
     public static String readFile(String path) {
